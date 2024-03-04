004488b0b3fa83eae8e4762ab8f5d19b5cc14d19
Dragonir44 <dragonir44@gmail.com>
2024-03-04 15:02:01 +0100

    Mise à jour du serveur.ts : Importation du module dotenv pour charger les variables d'environnement et définition du port d'écoute du serveur

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import dotenv from 'dotenv';
-import app from './app/index';
+import dotenv from 'dotenv'; // Importation du module dotenv pour charger les variables d'environnement
+import app from './app/index'; // Importation du module app depuis le dossier app/index
 
-// load environment variables
-dotenv.config()
 
-const PORT = process.env.PORT || 5000;
+// Charger les variables d'environnement à partir du fichier .env
+dotenv.config();
+
+const PORT = process.env.PORT || 5000; // Définition du port d'écoute du serveur, en utilisant la variable d'environnement PORT ou en utilisant le port 5000 par défaut
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`); // Affichage d'un message indiquant que le serveur est en cours d'exécution sur le port spécifié
 });
