be3c2476e239ce7f720f720f967f63a541742a40
Dragonir44 <dragonir44@gmail.com>
2024-03-04 15:01:50 +0100

    Mise à jour du modèle "channel" et du routeur Express

--- src/app/routes/channel.ts
+++ src/app/routes/channel.ts
@@ -1,28 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
-
 import {init as channel, getChannels, getChannel, getVersion, getDownload} from '../models/channel';
 
-channel()
+channel(); // Appel de la fonction d'initialisation du modèle "channel"
 
-dotenv.config()
-const router = express.Router();
+dotenv.config(); // Chargement des variables d'environnement depuis le fichier .env
+const router = express.Router(); // Création d'un routeur Express
 
 router.route('/')
     .get((req, res) => {
-        if (req.headers['token'] === process.env.TOKEN) {
-            res.json(getChannels());
+        if (req.headers['token'] === process.env.TOKEN) { // Vérification du token dans l'en-tête de la requête
+            res.json(getChannels()); // Renvoi des canaux disponibles au format JSON
         }
         else {
-            res.status(403).send('Forbidden');
+            res.status(403).send('Forbidden'); // Renvoi d'une erreur 403 si le token est incorrect ou manquant
         }
     })
 
 router.route('/:channel')
     .get((req, res) => {
         if (req.headers['token'] === process.env.TOKEN) {
-            res.json(getChannel(req.params.channel));
+            res.json(getChannel(req.params.channel)); // Renvoi des informations sur un canal spécifique au format JSON
         }
         else {
             res.status(403).send('Forbidden');
@@ -32,7 +31,7 @@ router.route('/:channel')
 router.route('/:channel/:version')
     .get((req, res) => {
         if (req.headers['token'] === process.env.TOKEN) {
-            res.json(getVersion(req.params.channel, req.params.version));
+            res.json(getVersion(req.params.channel, req.params.version)); // Renvoi des informations sur une version spécifique d'un canal au format JSON
         }
         else {
             res.status(403).send('Forbidden');
@@ -42,11 +41,11 @@ router.route('/:channel/:version')
 router.route('/:channel/:version/download')
     .get((req, res) => {
         if (req.headers['token'] === process.env.TOKEN) {
-            res.download(path.join(getDownload(req.params.channel, req.params.version)));
+            res.download(path.join(getDownload(req.params.channel, req.params.version))); // Téléchargement d'un fichier à partir du chemin spécifié
         }
         else {
             res.status(403).send('Forbidden');
         }
     })
 
-export default router;
+export default router; // Exportation du routeur Express par défaut
