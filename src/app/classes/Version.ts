a5785c9c11b71bfcbd963c1d26067947169ead30
Dragonir44 <dragonir44@gmail.com>
2024-03-04 15:01:39 +0100

    Mise à jour de la classe Version pour inclure des accesseurs pour la version, le journal des modifications et le chemin du fichier

--- src/app/classes/Version.ts
+++ src/app/classes/Version.ts
@@ -1,23 +1,23 @@
 export class Version {
-    protected version: string;
-    protected changelog: string;
-    protected path: string;
-    
+    protected version: string; // La version du logiciel
+    protected changelog: string; // Le journal des modifications
+    protected path: string; // Le chemin du fichier
+
     constructor(version: string, changelog: string, path: string) {
         this.version = version;
         this.changelog = changelog;
         this.path = path;
     }
 
-    get Version() {
+    get Version() { // Accesseur pour obtenir la version
         return this.version;
     }
 
-    get Changelog() {
+    get Changelog() { // Accesseur pour obtenir le journal des modifications
         return this.changelog;
     }
 
-    get Path() {
+    get Path() { // Accesseur pour obtenir le chemin du fichier
         return this.path;
     }
 }
